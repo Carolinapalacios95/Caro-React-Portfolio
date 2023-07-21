c80933e457b51f5f73c516a610e7d7485b2b4af5
Carolina <carolina.palacios1995@gmail.com>
2023-07-21 11:00:33 -0400

    added functionality to Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default function Navbar({ currentPage, handlePageChange }) {
+    return (
+        <div>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a
+                    href="#AboutMe"
+                    onClick={() => handlePageChange('AboutMe')}
+                    // This is a conditional (ternary) operator that checks to see if the current page is "AboutMe"
+                    // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                    className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+                >
+                Portfolio
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Contact"
+                    onClick={() => handlePageChange('Contact')}
+                   
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                Contact
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Resume"
+                    onClick={() => handlePageChange('Resume')}
+                    
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                Resume
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Projects"
+                    onClick={() => handlePageChange('Projects')}
+                   
+                    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                >
+                Projects
+                </a>
+            </li>
+        </ul>
+        </div>
+    );
+}
