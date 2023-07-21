60e4f55d48efae7e395771cdafbbdff1193dae3b
Carolina <carolina.palacios1995@gmail.com>
2023-07-21 10:42:34 -0400

    added Resume page component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,1 +1,31 @@
 import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <h2>Front-End Proficiencies</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h2>Back-End Proficiencies</h2>
+            <ul>
+                <li>GraphQL</li>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>Progressive Web Applications (PWA)</li>
+                <li>Object Oriented Programming (OOP)</li>
+                <li>Object-Relational Mapping (ORM)</li>
+            </ul>
+        </div>
+    );
+}
