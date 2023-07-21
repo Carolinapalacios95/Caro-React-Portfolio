896e48d9ca07b72391baa6a48ed94661dfb6c2fe
Carolina <carolina.palacios1995@gmail.com>
2023-07-21 11:28:27 -0400

    added content & logos to Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <div>
+            <a href="https://github.com/Carolinapalacios95/" target="_blank" rel="noopener noreferrer">
+                <svg className="github" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47"/>
+                </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/carolina-bermeo-palacios-920b8185/" target="_blank" rel="noopener noreferrer">
+                <svg className="linkedin" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M0 2.5C0 1.122 1.122 0 2.5 0h11"/>
+                </svg>
+            </a>
+        </div>
+    )
+}
+
